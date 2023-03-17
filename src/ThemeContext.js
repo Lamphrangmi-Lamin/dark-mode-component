98d0b19f846b09dcc5a1cc695011e5f88b89368d
Lamphrangmi-Lamin <lamphrangmilamin46169@gmail.com>
2023-03-17 09:18:30 +0530

    first commit

---
+++ src/ThemeContext.js
@@ -0,0 +1,20 @@
+import { createContext } from "react";
+import { useContext } from "react";
+import { useState } from "react";
+const ThemeContext = createContext(undefined);
+
+export const ThemeProvider = ({ children }) => {
+    const [theme, setTheme] = useState('light');
+    return (
+        <ThemeContext.Provider 
+            value={{
+                theme,
+                toggleTheme: () => setTheme(theme === "light" ? "dark" : "light")
+            }}
+        >
+            {children}
+        </ThemeContext.Provider>
+    )
+};
+
+export const useTheme = () => useContext(ThemeContext);
